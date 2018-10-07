3e9a0c021280554e724f2fe30614c6ce367700a6
sharafian <ben@sharafian.me>
2018-10-07 15:55:04 -0700

    feat: monetize it

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { NodeMediaServer } = require('node-media-server');
+const stream = require('stream')
+const Koa = require('koa')
+const httpProxy = require('http-proxy')
+const http = require('http')
+const router = require('koa-router')()
+const serve = require('koa-static')
+const { Monetizer } = require('web-monetization-receiver')
+const path = require('path')
  
 const config = {
   logType: 3,
- 
   rtmp: {
     port: 1935,
     chunk_size: 60000,
@@ -17,46 +24,37 @@ const config = {
 };
  
 const nms = new NodeMediaServer(config)
+const proxy = new httpProxy.createProxy({})
+const app = new Koa()
+const monetizer = new Monetizer()
 
-nms.run();
-nms.on('preConnect', (id, args) => {
-  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
- 
-nms.on('postConnect', (id, args) => {
-  console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
- 
-nms.on('doneConnect', (id, args) => {
-  console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
- 
-nms.on('prePublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
- 
-nms.on('postPublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
- 
-nms.on('donePublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
- 
-nms.on('prePlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
- 
-nms.on('postPlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
- 
-nms.on('donePlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
+router.get('/live/*', ctx => {
+  const pass = new stream.PassThrough()
+  const monetized = ctx.webMonetization.monetizeStream(pass)
+  monetized.pipe(ctx.res)
+
+  const reqProxy = new Proxy({}, {
+    get: function (obj, prop) {
+      console.log('GET PROPERTY', prop)
+      return pass[prop] || ctx.res[prop]
+    }
+  })
+
+  proxy.web(ctx.req, reqProxy, {
+    target: 'http://localhost:8000'
+  })
+
+  ctx.status = 200
+  return new Promise(resolve => {
+    ctx.res.on('end', resolve)
+  })
+})
+
+nms.run()
+
+app
+  .use(monetizer.koa())
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(serve(path.resolve(__dirname, 'static')))
+  .listen(8080)
